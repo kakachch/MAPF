6f0c3c89c8aff03c5729ed4e7f0ba497d7abb147
pengxiankang <718257480@qq.com>
2024-03-21 09:25:13 +0800

    change name to schedule

---
+++ task_schedule/include/task_schedule.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef TASK_ASSIGNMENT_H
+#define TASK_ASSIGNMENT_H
+#include <mapf_environment/base_environment.h>
+//任务的起始点和目标点
+struct task
+{
+    int start_location;
+    int goal_location;
+    task(): start_location(0),goal_location(0){}
+    task(const int &start_location,const int &goal_location): start_location(start_location), goal_location(goal_location){}
+    
+};
+//robot的位置、方向、以及种类（ctu/agv）
+struct robot
+{
+    int location;
+    int orientation;
+    int type;
+    robot(): location(0), orientation(0), type(){}
+    robot(const int &location, const int &orientation, const int &type): location(location), orientation(orientation), type(type){}
+    robot(const Pose &pose,const int &type) : location(pose.location), orientation(pose.orientation), type(type){}
+};
+
+#endif
